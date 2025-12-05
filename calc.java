a13b81749334a81d4da1bd090cf1086d4eb2daa8
mitra dehghani beshneh <mitra.dehghanibsh@gmail.com>
2025-12-05 14:37:26 +0200

    Add files via upload

---
+++ calc.java
@@ -0,0 +1,25 @@
+class calculator {
+
+ int num1,num2 ,addition_result,subtraction_result;
+
+ public int addition(int num1,int num2)
+ {
+  return num1+num2;
+ }
+ 
+ public int subtraction(int num1,int num2)
+ {
+  return num2-num1;
+ }
+ 
+
+ public double division(int num1,int num2)
+ {
+       return num1 / num2;
+ }
+
+ public int multiple (int num1,int num2)
+ {
+     return num1*num2 ;
+ }
+}
