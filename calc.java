3119f116272832c84e99af53f50b43d81fb0e891
MD Ishrak Rahman <md.rahman@student.hamk.fi>
2025-12-05 19:14:59 +0200

    comments
    I have added comments to the code.

--- calc.java
+++ calc.java
@@ -1,25 +1,25 @@
-class calculator {
+class calculator {          //The class is calculator
 //attributes for the caculator
  int num1,num2 ,addition_result,subtraction_result;
 
  //method for adding the two numbers
- public int addition(int num1,int num2)
+ public int addition(int num1,int num2)          //here we are adding two numbers
  {
-  return num1+num2;
+  return num1+num2;          //returning num 1 and num 2
  }
  // method for sub two numbers
- public int subtraction(int num1,int num2)
+ public int subtraction(int num1,int num2)      //here subtraction happens
  {
   return num2-num1;
  }
  
 
- public double division(int num1,int num2)
+ public double division(int num1,int num2)    //here we divide numbers
  {
        return num1 / num2;
  }
 
- public int multiple (int num1,int num2)
+ public int multiple (int num1,int num2)          //here multiplication happens
  {
      return num1*num2 ;
  }
